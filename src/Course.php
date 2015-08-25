62262fa8c5c0ee7180ab69505dd4ab9b9a368eb1
Casey Heitz & Jeff Franco <student@epicodus.com>
2015-08-25 15:59:56 -0700

    Add passing tests for update, find, delete Courses

--- src/Course.php
+++ src/Course.php
@@ -17,6 +17,17 @@
             return $this->course_name;
         }
 
+        function setCourseName($new_course_name)
+        {
+            $this->course_name = $new_course_name;
+
+        }
+
+        function setCourseCode($new_course_code)
+        {
+            $this->course_code = $new_course_code;
+        }
+
         function getCourseCode()
         {
             return $this->course_code;
@@ -34,6 +45,20 @@
             $this->id=$GLOBALS['DB']->lastInsertId();
         }
 
+        function delete()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM courses WHERE id = {$this->getId()};");
+
+        }
+
+        function update($new_course_name, $new_course_code)
+        {
+            $GLOBALS['DB']->exec("UPDATE courses SET course_name = '{$new_course_name}', course_code = '{$new_course_code}' WHERE id = {$this->getId()};");
+            $this->setCourseName($new_course_name);
+            $this->setCourseCode($new_course_code);
+
+        }
+
         static function getAll()
         {
             $returned_courses = $GLOBALS['DB']->query("SELECT * FROM courses ORDER BY course_name;");
@@ -48,6 +73,22 @@
             }
 
             return $courses;
+        }
+
+        static function find($search_id)
+        {
+            $found_course = null;
+            $courses = Course::getAll();
+            foreach($courses as $course)
+            {
+                $course_id = $course->getId();
+                if($course_id == $search_id) {
+                    $found_course = $course;
+                }
+
+            }
+
+            return $found_course;
         }
 
         static function deleteAll()
--- tests/CourseTest.php
+++ tests/CourseTest.php
@@ -82,6 +82,62 @@
             $this->assertEquals([$test_course2, $test_course], $result);
 
         }
+
+        function testFind()
+        {
+            $course_name = "History";
+            $course_code = "HIST100";
+            $test_course = new Course($course_name, $course_code);
+            $test_course->save();
+
+            $course_name2 = "Gym";
+            $course_code2 = "GYM100";
+            $test_course2 = new Course($course_name2, $course_code2);
+            $test_course2->save();
+
+            $result = Course::find($test_course->getId());
+
+            $this->assertEquals($test_course, $result);
+
+        }
+
+        function testDelete()
+        {
+            $course_name = "History";
+            $course_code = "HIST100";
+            $test_course = new Course($course_name, $course_code);
+            $test_course->save();
+
+            $course_name2 = "Gym";
+            $course_code2 = "GYM100";
+            $test_course2 = new Course($course_name2, $course_code2);
+            $test_course2->save();
+
+            $test_course->delete();
+
+            $this->assertEquals([$test_course2], Course::getAll());
+
+        }
+
+        function testUpdate()
+        {
+            $course_name = "History";
+            $course_code = "HIST100";
+            $test_course = new Course($course_name, $course_code);
+            $test_course->save();
+
+            //Creatin a course with values of History, HIST100, and id from database
+
+            $new_course_name = "Math";
+            $new_course_code = "MATH100";
+            $new_course = new Course($new_course_name, $new_course_code, $test_course->getId());
+            //creating a new course with values of Math, MATH100, and id that matches test_course
+
+            $test_course->update($new_course_name, $new_course_code);
+
+            $this->assertEquals($test_course, $new_course);
+
+        }
     }
 
 
